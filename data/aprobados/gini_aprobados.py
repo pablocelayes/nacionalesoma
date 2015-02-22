05344b377f7b166031b12dc17a7f3cf24aa1262b
Pablo G Celayes <pablocelayes@gmail.com>
2015-02-22 23:31:29 +0100

    Agregué cálculo de índice de Gini sobre provincias para clasificados y aprobados

---
+++ data/aprobados/gini_aprobados.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Índice de Gini histórico para
+    cantidades de aprobados por provincia
+"""
+import pandas as pd
+# from utils import gini
+def gini(values):
+    values = sorted(values, key=lambda x:-x)
+    n = len(values)
+
+    uneven = sum([(n - i) * v for (i, v) in enumerate(values)])
+    even = 0.5 * n * sum(values)
+
+    return (uneven - even) / even
+
+
+años = list(range(1998, 2015))
+
+años.remove(2010)
+# TODO: arreglar 2010
+
+csvfile = 'csvs/aprobados%d.csv'
+
+ginis = []
+
+for año in años:
+    df = pd.read_csv(csvfile % año, parse_dates=True)
+    provcounts = df.groupby(['Provincia']).size()
+    ginis.append([año, gini(provcounts.values)])
+
+df = pd.DataFrame(columns=["Año", "Gini"], data=ginis)
+df.to_csv("gini_aprobados.csv", index=False)
---
+++ data/clasificados/gini_clasificados.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Índice de Gini histórico para
+    cantidades de clasificados por provincia
+"""
+import pandas as pd
+# from utils import gini
+def gini(values):
+    values = sorted(values, key=lambda x:-x)
+    n = len(values)
+
+    uneven = sum([(n - i) * v for (i, v) in enumerate(values)])
+    even = 0.5 * n * sum(values)
+
+    return (uneven - even) / even
+
+
+años = range(1998, 2015)
+csvfile = 'csvs/clasificados%d.csv'
+
+ginis = []
+
+for año in años:
+    df = pd.read_csv(csvfile % año, parse_dates=True)
+    provcounts = df.groupby(['Provincia']).size()
+    ginis.append([año, gini(provcounts.values)])
+
+df = pd.DataFrame(columns=["Año", "Gini"], data=ginis)
+df.to_csv("gini_clasificados.csv", index=False)
