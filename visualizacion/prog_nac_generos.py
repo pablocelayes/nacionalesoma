1e5dab08853c1ec70441c78b272bd5c25a80f2ac
Alejandro Zamora <ale2014.zamora@gmail.com>
2015-08-18 18:12:49 -0400

    Generados los svg de progresion femenina por provincias y categorías.

--- visualizacion/prog_nac_generos.py
+++ visualizacion/prog_nac_generos.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
 files = ["../data/aprobados/csvs/aprobados_por_género.csv",
@@ -13,7 +12,7 @@ files = map(pd.read_csv,files)
 
 for cat,file in zip(["aprobados","clasificados","premiados"],files):
 	fig = plt.figure()
-	plt.bar(file['Año'],file['F'],color='#ff6496')
+	plt.bar(file.groupby('Año').size(),file['F'],color='#ff6496')
 	plt.title('Progresión femenina nacional de '+cat+'.')
 	plt.ylabel('Cantidad')
 	plt.savefig("./plots/genero/F/progresion_anual_"+cat+".svg")
