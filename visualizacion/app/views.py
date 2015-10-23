7f0129b570006cdd61c539d955deab5a84c7054d
Alejandro Zamora <terefv@ltu.sld.cu>
2015-10-23 11:53:33 -0400

    Refactorizando, comenzando a modularizar, obteniendo toda la data de los años en el server

--- visualizacion/app/views.py
+++ visualizacion/app/views.py
@@ -10,9 +10,10 @@ import numpy as np
 import xlrd
 import re
 # from os import getcwd
-from random import random
+import matplotlib.pyplot as plt
 
 caba = re.compile(r'Capital Federal|^.+Buenos Aires$')
+file_cat_counts = '../data/%s/provcounts.csv'
 
 def is_CABA(string):
 	return caba.search(string)
@@ -36,7 +37,7 @@ def get_pob_esc1(year):
 	
 	file1 = '../data/selected_pob_escolar/%s/EGB3.xls'
 	file2 = '../data/selected_pob_escolar/%s/POLIMODAL.xls'
-	file_clasif = '../data/clasificados/provcounts.csv'
+	
 	
 	book1 = xlrd.open_workbook(file1 % year)
 	book2 = xlrd.open_workbook(file2 % year)
@@ -49,27 +50,17 @@ def get_pob_esc1(year):
 		
 	book2_provs = list(map(check,sheet_book2.col_values(0)[4:28]))
 	book2_counts = sheet_book2.col_values(1)[4:28]
-	
-	df_clasif = read_csv(file_clasif)
-	
-	df_clasif = df_clasif[df_clasif['Año'] == year]
-	
-	df_clasif = DataFrame({'Provincia':df_clasif['Provincia'],
-						'Clasificados':df_clasif['Cantidad']})
 					
 	
 	df_pob_esc = DataFrame({'Provincia':book1_provs,
-					'Población':np.array(book1_counts)+np.array(book2_counts),
-					})
+							'Población':np.array(book1_counts)+np.array(book2_counts),
+						   })
 	
-	df = merge(df_pob_esc,df_clasif,on='Provincia',how='left')
-		
-	return df.fillna(value=0)
+	return df_pob_esc
 
 def get_pob_esc2(year):					
 	
 	file = '../data/selected_pob_escolar/%s/COMUN.xls'
-	file_clasif = '../data/clasificados/provcounts.csv'
 	
 	book = xlrd.open_workbook(file % year)
 	sheet_book = book.sheets()[0]
@@ -79,24 +70,53 @@ def get_pob_esc2(year):
 	book_counts_1 = sheet_book.col_values(4)[11:35]
 	book_counts_2 = sheet_book.col_values(8)[11:35]
 	
-	df_clasif = read_csv(file_clasif)
-	
-	df_clasif = df_clasif[df_clasif['Año'] == year]
-	
-	df_clasif = DataFrame({'Provincia':df_clasif['Provincia'],
-						'Clasificados':df_clasif['Cantidad']})
 	
 	df_pob_esc = DataFrame({'Provincia':book_provs,
-					'Población':np.array(book_counts_1)+np.array(book_counts_2)})
+							'Población':np.array(book_counts_1)+np.array(book_counts_2)})
 	
-	df = merge(df_pob_esc,df_clasif,on='Provincia',how='left')
-		
-	return df.fillna(value=0)
+	return df_pob_esc
+
+def get_cat(cat,year):
+	df_cat = read_csv(file_cat_counts % cat)
+	
+	df_cat = df_cat[df_cat['Año'] == year]
+	
+	cap_cat = cat.capitalize()
+	
+	df_cat = DataFrame({'Provincia':df_cat['Provincia'],
+						cap_cat:df_cat['Cantidad']})		
+	
+	return df_cat
+
+def combine_cat_with_pob_esc(cat,year):
+	
+	df_pob_esc = poblacion_escolar(year)
+	
+	df_cat = get_cat(cat,year)
+	
+	df = merge(df_pob_esc,df_cat,on='Provincia',how='left')
+	
+	df = df.fillna(value=0)	
+	
+	return df	
+	
+def all_years():
+	res = {}
+	for year in range(1998,2015):
+		df_clasif = combine_cat_with_pob_esc("clasificados",year)
+		df_aprob  = combine_cat_with_pob_esc("aprobados",year)
+		df_tmp = merge(df_clasif,df_aprob,on=['Provincia','Población'],how='outer')
+		res[year] = df_tmp
+	return res
+
+data = all_years()
+
+# pprint(data)
 
 def save_div(m,n):
 	if n != 0:
 		return m/n
-	return 0	
+	return 0
 	
 def df_to_response(df,colores):
 	tmp = df.to_dict()
@@ -109,20 +129,23 @@ def df_to_response(df,colores):
 	def color(valor):
 		i = math.floor((len(colores)-1) * (math.log(valor + 1) - min_value) / (max_value - min_value))
 		return colores[i]
-	
+		
 	res = {}
 	for i in range(24):
-		n = tmp['Población'][i]
 		m = tmp['Clasificados'][i]
+		n = tmp['Población'][i]
+		o = tmp['Aprobados'][i]
 		index = save_div(m,n)	
 		res[tmp['Provincia'][i].strip()] = {'Población':n,
-									'Clasificados':m,
-									'Índice':index,
-									'Color':color(index)}
-									#agregar después(quizá en otra parte)
-									#los datos por niveles
+											'Clasificados':m,
+											'Aprobados':o,
+											'Índice':index,
+											'Color':color(index)}
+											#agregar después(quizá en otra parte)
+											#los datos por niveles
 	return res
-			
+	
+	
 @app.route('/',methods=['GET'])
 @app.route('/index',methods=['GET'])
 def index():
@@ -146,7 +169,7 @@ def update():
 	colores = ["#ffe9e9","#e0b6b6","#db9696",
 				"#7e4747","#410e0e"]
 	year = int(request.form['year'])
-	df = poblacion_escolar(year)
+	df = data[year]
 	# print(df)
 	val = df_to_response(df,colores)
 	pprint(val)
---
+++ visualizacion/processing/prog_part_provs.py
@@ -0,0 +1,61 @@
+﻿# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------
+#Vamos viendo como salen los svg's			 	|		
+
+# def prog_part_aprob_clasif(prov):
+	# """
+	# Genera un gráfico de 
+	# barras en svg con los porcientos de los
+	# clasificados/aprobados de los años 
+	# respecto a su población escolar.
+	# """
+	# df = data[year]
+	# df = 
+	# clasif_percent = df['Clasificados']/df['Población']
+	# aprob_percent  = df['Aprobados']/df['Población']
+	
+	# fig = plt.figure()
+	# # plt.bar(file.groupby('Año').size(),file['F'],color='#ff6496')
+	# plt.bar(file['Año'],file['F'],color='#00ffff')
+	# plt.title('Participación'+year+'.')
+	# plt.ylabel('%')
+	# plt.savefig("part%d.svg" % year)
+	# # plt.show()
+	# plt.close(fig)	
+	
+#												|	
+#-----------------------------------------------
+
+aprobados_patch = mpatches.Patch(color='#3F7F7F', label='Aprobados')
+
+files = ["../../data/aprobados/provcounts.csv",
+		 "../../data/clasificados/provcounts.csv",]
+	 
+		 
+aprobados_prov,clasificados_prov = map(pd.read_csv,files)
+
+x = np.arange(1998,2015)
+
+
+provincias = set(aprobados_prov['Provincia'])
+
+for i in provincias:
+	aprobados_rows = aprobados_prov[aprobados_prov['Provincia'] == i]
+	clasificados_rows = clasificados_prov[clasificados_prov['Provincia'] == i]
+	fig = plt.figure()
+	print(i)
+	# #3F7F7F
+	plt.bar(aprobados_rows['Año'],aprobados_rows['Cantidad'],color='blue',alpha=0.5)
+	graph2 = plt.bar(clasificados_rows['Año'],clasificados_rows['Cantidad'],label='Clasificados',color='green',alpha=0.5)
+	plt.title('Progresión provincia de '+i)
+	plt.ylabel('Cantidad')
+	plt.legend(handles=[aprobados_patch, graph2])
+	plt.savefig("../app/static/img/plots/"+i.replace(" ","_")+"-completo.svg")
+	# plt.show()
+	plt.close(fig)
