7e985155aeacc70c3e05a783dac354bb0b23d179
Alejandro Zamora <terefv@ltu.sld.cu>
2015-11-19 13:09:49 -0500

    Casi completa la carga en el cliente y visualización en vista participacion

---
+++ visualizacion/app/static/js/participacion.js
@@ -0,0 +1,78 @@
+﻿function begin_participacion(){
+    var provinces = 
+    ["Buenos Aires",
+     "Catamarca",
+     "Chaco",
+     "Chubut",
+     "Ciudad Autónoma de Buenos Aires",
+     "Corrientes",
+     "Córdoba",
+     "Entre Ríos",
+     "Formosa",
+     "Jujuy",
+     "La Pampa",
+     "La Rioja",
+     "Mendoza",
+     "Misiones",
+     "Neuquén",
+     "Río Negro",
+     "Salta",
+     "San Juan",
+     "San Luis",
+     "Santa Cruz",
+     "Santa Fe",
+     "Santiago del Estero",
+     "Tierra del Fuego",
+     "Tucumán"
+    ];
+
+	var mini_svgs;
+	var completos_svgs;
+	
+	var svg_objects = {'mini':[],'completo':[]};
+	
+    function fill_svg_objects(){
+    var svg = "/static/img/plots/poblacion_escolar/"
+    for (var i = 0; i < provinces.length; i++) 
+    {
+            d3.xml(svg+provinces[i].replace(/\s/g, '_')+".svg","image/svg+xml", 
+           function(xml)
+           {
+               svg_objects['mini'].push(xml);
+               if(svg_objects['mini'].length == 24)
+               // ya se han cargado todos los svgs      
+               {
+               mini_svgs = to_svg_provinces('mini');
+               }
+           });
+    } 
+    for (var j = 0; j < provinces.length; j++)
+    {
+    		d3.xml(svg+provinces[j].replace(/\s/g, '_')+"-completo.svg"
+	   ,"image/svg+xml", 
+	   function(xml)
+	   {
+		   svg_objects['completo'].push(xml);
+		   if(svg_objects['completo'].length == 24)
+		   // ya se han cargado todos los svgs      
+		   {
+		   completos_svgs = to_svg_provinces('completo');
+		   return init_participacion(mini_svgs,completos_svgs);
+		   }
+	   });
+
+	
+    }
+	}	
+    
+    function to_svg_provinces(type){
+        var res = {};
+        for (var i = 0; i < provinces.length; i++)
+        {
+            res[provinces[i]] = svg_objects[type][i];
+        }
+        return res;
+    }
+	
+	fill_svg_objects();
+}
---
+++ visualizacion/app/static/js/patch_cbpFWTabs.js
@@ -0,0 +1,54 @@
+ntabs = 4
+
+function next_tab(n){
+	if(n<ntabs-1)
+		return n+1;
+	return 0;
+}
+
+function prev_tab(n){
+	if(n>0)
+		return n-1;
+	return ntabs-1;
+}
+
+prev = d3.select("#prev_tab");
+next = d3.select("#next_tab");
+
+the_tabs = d3.select("#tabs").select("nav")
+							 .select("ul")
+							 .selectAll("li");
+							 
+the_contents = d3.select(".content").selectAll("section");							 
+
+prev.on("click",function(){update_tabs(prev_tab(current_tab()))});
+next.on("click",function(){update_tabs(next_tab(current_tab()))});
+							 
+function current_tab(){
+	for(i=0;i<the_tabs[0].length;i++){
+		if (the_tabs[0][i].className != "")
+			return i;
+	}
+}
+
+function update_tabs(j){
+	// alert(j);
+	for(i=0;i<the_tabs[0].length;i++){
+		if (i!=j){
+			the_tabs[0][i].className = "";
+			the_contents[0][i].className = "";
+		}	
+		else{
+			the_tabs[0][i].className = "tab-current"; 	
+			the_contents[0][i].className = "content-current";
+		}		
+	}
+}							 
+
+function load_all_vis(){
+	mapa_node.style("display","block");	
+	add_years("Clasificados");
+	cat_list_gen.property("value","Clasificados");
+	begin_participacion();
+	// f.filtrar_poblacion_esc(1998,f_click_pob_esc);
+}
