f095d9065ad0dd34fb29c59b3c7e28d24035a40d
Alejandro Zamora <allejjandrozf@gmail.com>
2016-02-20 17:26:09 -0300

    Cambiando el scalado "directo" en el tooltip. Trabajo en los ejes. Añadiendo descripción de la data con colores.

--- visualizacion/app/static/js/participacion.js
+++ visualizacion/app/static/js/participacion.js
@@ -121,8 +121,8 @@ function participacion(years_partic,paths){
 
     function paint_svg(tooltip_node,prog_prov){
 	
-	var w = 500;
-	var h = 200;
+	var w_init = 500;
+	var h_init = 200;
 	var barPadding = 5;
 	var width = 500,
 	margin = 25,
@@ -130,42 +130,45 @@ function participacion(years_partic,paths){
 	axisWidth = width - 2 * margin;
 	
 	var padding = 20;
+	var shrink_factor = 100;
 	
 	var scale = d3.scale.linear();
-	
+
+	var svg = tooltip_node.append("svg")
+	    .attr("width", w_init)
+	    .attr("height", h_init);	
+
 	scale.domain([0,d3.max(prog_prov['data'],
 			       function(d){
 				   return d['Índice']; 
 			       })]);
+	
+	h = h_init - shrink_factor;
+	w = w_init - shrink_factor;
+
 	scale.range([0,h]);
 	
 	scale.clamp();
-	
-	var svg = tooltip_node.append("svg")
-	    .attr("width", w)
-	    .attr("height", h);
 
-	svg.append("g").attr("transform","translate(10,10) scale(0.8)")
-	    .append("line")
-	    .attr({
-		x1: 0,
-		y1: 0,
-		x2: 0,
-		y2: h,
-		stroke: "#CCC"
-	    });
+	svg.append("g").attr("transform","translate(30,30)")
+	    .selectAll("rect")
+	    .attr("transform", "translate(20,20)")
+	    .data(prog_prov['data'])
+	    .enter()
+	    .append("rect")
+	    .attr("x", function(d, i) {
+		return i * (w / prog_prov['data'].length); 
+	    })
+	    .attr("y", function(d) {
+		return h - scale(d['Aprobados']/d['Población']);
+	    })
+	    .attr("width", w / prog_prov['data'].length - barPadding)
+	    .attr("height", function(d) {
+		return scale(d['Aprobados']/d['Población'])
+	    })
+	    .attr("fill","#b79191");
 
-	svg.append("g").attr("transform","translate(10,10) scale(0.8)")
-	    .append("line")
-	    .attr({
-		x1: 0,
-		y1: h,
-		x2: w,
-		y2: h,
-		stroke: "#CCC"
-	    });
-
-	svg.append("g").attr("transform","translate(20,20) scale(0.7)")
+	svg.append("g").attr("transform","translate(30,30)")
 	    .selectAll("rect")
 	    .attr("transform", "translate(20,20)")
 	    .data(prog_prov['data'])
@@ -182,25 +185,28 @@ function participacion(years_partic,paths){
 	    .attr("height", function(d) {
 		return scale(d['Índice'])
 	    })
-	    .attr("fill","#410e0e");
+	    .attr("fill","#e0b6b6");
 
-	svg.append("g").attr("transform","translate(20,20) scale(0.7)")
-	    .selectAll("rect")
-	    .attr("transform", "translate(20,20)")
-	    .data(prog_prov['data'])
-	    .enter()
-	    .append("rect")
-	    .attr("x", function(d, i) {
-		return i * (w / prog_prov['data'].length); 
-	    })
-	    .attr("y", function(d) {
-		return h - scale(d['Aprobados']/d['Población']);
-	    })
-	    .attr("width", w / prog_prov['data'].length - barPadding)
-	    .attr("height", function(d) {
-		return scale(d['Aprobados']/d['Población'])
-	    })
-	    .attr("fill","#brown");
+	svg.append("g").attr("transform","translate(20,30)")
+	    .append("line")
+	    .attr({
+		x1: 0,
+		y1: 0,
+		x2: 0,
+		y2: h+10,
+		stroke: "#CCC"
+	    });
+
+	svg.append("g").attr("transform","translate(20,30)")
+	    .append("line")
+	    .attr({
+		x1: 0,
+		y1: h+10,
+		x2: w+20,
+		y2: h+10,
+		stroke: "#CCC"
+	    });
+
 	
     }
 
@@ -223,19 +229,13 @@ function participacion(years_partic,paths){
 	    	"<p><b>Provincia:</b> "+path_to_provs[id]+"</p>"+ 
 	    	"<p><b>Poblabión escolar: </b>"+
 	    	data_json[prov_name]['Población']+"</p>"+
-	    	"<p>"+data_json[prov_name]['Clasificados']+" clasificados(s)</p>"+
-	    	"<p>"+data_json[prov_name]['Aprobados']+" aprobado(s)</p>"+
+	    	"<p>"+data_json[prov_name]['Clasificados']+" clasificados(s) <div class='min-square-clasif'></div></p>"+
+	    	"<p>"+data_json[prov_name]['Aprobados']+" aprobado(s) <div class='min-square-aprob'></div></p>"+
 	    	"<p>Progresión respecto a población escolar:</p>";
 
 	    tooltip_node.html(content);
 
-	    paint_svg(tooltip_node,prog_prov);
-	    
-	    // svg_val = "<svg width='600' height='600'>"+ mini_svgs[prov_name].documentElement.innerHTML+"</svg>";
-	    
-
-	    
-
+	    paint_svg(tooltip_node,prog_prov)	    
 	    tooltip_node.style("display","block");
 	}}
 
